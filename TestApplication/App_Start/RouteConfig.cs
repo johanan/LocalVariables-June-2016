cc913d65784966c75ba68a8d1bb4b2d523359854
Joshua Johanan <josh359@gmail.com>
2016-06-11 21:47:35 -0400

    Created a new application

---
+++ TestApplication/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(TestApplication.App_Start.RouteConfig), "StartUpRoutes")]
+namespace TestApplication.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        public static void StartUpRoutes()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ TestApplication/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using LocalVariables.Arbitrary;
+
+namespace TestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.SayMyName = ArbitraryClass.ArbitraryMethod("Josh");
+            return View();
+        }
+    }
+}
